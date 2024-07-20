d496d28da70aa4ddb9c7e930e2d579f457b85e68
ujjawal-mukherjee <ujjawalkumarmukherjee335@gmail.com>
2024-07-20 09:59:53 +0530

    Update script.js

--- hospital website/script.js
+++ hospital website/script.js
@@ -1,7 +1,31 @@
 let menubar = document.querySelector('#menu-bars');
-let navbar  = document.querySelector('.navbar');
+let navbar = document.querySelector('.navbar');
 
-menubar.onclick = () =>{
-    menubar.classList.toggle('fa-times');
+menubar.onclick = () => {
+    /*menubar.classList.toggle('fa-times');*/
     navbar.classList.toggle('active')
 }
+let readmore = document.querySelector('.tab');
+readmore.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const contentDiv = document.getElementById('content');
+
+    if (contentDiv.innerHTML === '') {
+        contentDiv.innerHTML = `
+            <p>
+                The transition from traditional paper prescriptions to electronic systems offers numerous benefits.
+                Paper prescriptions are prone to errors due to illegible handwriting,
+                misinterpretation, and physical loss or damage. E-Prescriptions eliminate these issues by providing
+                clear,
+                legible, and easily accessible digital records.
+                This not only enhances the accuracy of prescriptions but also reduces the administrative burden on
+                healthcare providers, allowing them to focus more on patient care.
+            </p>
+        `;
+        contentDiv.style.display = 'block';
+    } else {
+        contentDiv.style.display = contentDiv.style.display === 'none' ? 'block' : 'none';
+    }
+    readmore.remove();
+});
