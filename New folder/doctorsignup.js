61ad68c5cbf22124b045f52dc565b2ea7bbec3d5
ujjawal-mukherjee <ujjawalkumarmukherjee335@gmail.com>
2024-08-05 01:57:46 +0530

    Add files via upload

---
+++ New folder/doctorsignup.js
@@ -0,0 +1,5 @@
+document.getElementById('signUpForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Here you can handle the form data and send it to the server if needed
+    alert('Form submitted successfully!');
+});
